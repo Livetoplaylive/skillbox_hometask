647929cff63fb0f8429bee6971b141b5b6b3de42
Paul <svenwingame@gmail.com>
2021-06-14 03:36:55 +0300

    task three added

---
+++ 12/Self-isolation level/cmake-build-debug/CMakeFiles/Self_isolation_level.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Self_isolation_level.dir/main.cpp.o"
+  "Self_isolation_level"
+  "Self_isolation_level.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Self_isolation_level.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
