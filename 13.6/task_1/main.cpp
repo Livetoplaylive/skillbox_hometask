6abb1e8ac5cbf36d041bc4b68405c5d5a4be3668
paul <svenwingame@gmail.com>
2021-07-12 22:30:13 +0300

    task 13 reworked

--- 13.6/task_1/main.cpp
+++ 13.6/task_1/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]) {
 
     int range=atoi(argv[1]);
     int delete_num=atoi(argv[3]);
+    int num{};
     std::string temp=argv[2];
     std::vector<int>container;
 
@@ -18,39 +19,32 @@ int main(int argc, char *argv[]) {
         system("pause");
         return 0;
     }
-    container=contain_add(temp,range);
-    container=contain_delete(container,delete_num);
-
-    for (int i = 0; i <container.size(); ++i) {
-        std::cout<<container[i]<<std::endl;
-    }
-
-    return 0;
-}
-std::vector<int> contain_add( std::string temp,int range) {
-    int num{};
-    std::vector<int>tmp;
 
     for (int i = 0; i < temp.length(); ++i) {
+
         if(isdigit(temp[i])){
             num+=temp[i]-'0';
             num*=10;
         }
         else{
             num/=10;
-            tmp.push_back(num);
+            container.push_back(num);
+            if (num==delete_num){
+                container.pop_back();
+            }
             num=0;
-        }
-    }
-    return tmp;
-}
-std::vector<int> contain_delete(std::vector<int>tmp, int delete_num) {
 
-    int size=tmp.size();
-    for (int i = 0; i < size; ++i) {
-        if(tmp[i]==delete_num){
-            tmp.erase(tmp.begin()+i);
+
         }
+
     }
-    return tmp;
+
+
+    for (int i = 0; i <container.size(); ++i) {
+
+        std::cout<<container[i]<<std::endl;
+    }
+
+    return 0;
 }
+
