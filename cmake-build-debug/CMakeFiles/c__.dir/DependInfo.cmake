f9ba55a6c90cd050bfe9b1830aafe445555584ca
wandsman <svenwingame@gmail.com>
2021-08-05 20:57:30 +0300

    task 2 added

--- cmake-build-debug/CMakeFiles/c__.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/c__.dir/DependInfo.cmake
@@ -1,18 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/paul/Repositories/c++/11.6/Caesar_cipher/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/11.6/Caesar_cipher/main.cpp.o"
-  "/home/paul/Repositories/c++/11.6/comparison_numbers/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/11.6/comparison_numbers/main.cpp.o"
-  "/home/paul/Repositories/c++/11.6/email_correct/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/11.6/email_correct/main.cpp.o"
-  "/home/paul/Repositories/c++/11.6/ip_validation/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/11.6/ip_validation/main.cpp.o"
-  "/home/paul/Repositories/c++/11.6/tic_tac_toe/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/11.6/tic_tac_toe/main.cpp.o"
-  "/home/paul/Repositories/c++/12/Average number of storeys/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/12/Average_number_of_storeys/main.cpp.o"
-  "/home/paul/Repositories/c++/12/Self-isolation level/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/12/Self-isolation_level/main.cpp.o"
-  "/home/paul/Repositories/c++/12/task_1/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/12/task_1/main.cpp.o"
-  "/home/paul/Repositories/c++/empty_app/main.cpp" "/home/paul/Repositories/c++/cmake-build-debug/CMakeFiles/c__.dir/empty_app/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/11.6/Caesar_cipher/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/11.6/Caesar_cipher/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/11.6/comparison_numbers/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/11.6/comparison_numbers/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/11.6/email_correct/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/11.6/email_correct/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/11.6/ip_validation/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/11.6/ip_validation/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/11.6/tic_tac_toe/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/11.6/tic_tac_toe/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/12/Average number of storeys/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/12/Average_number_of_storeys/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/12/Self-isolation level/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/12/Self-isolation_level/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/12/task_1/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/12/task_1/main.cpp.o"
+  "/home/wandsman/repositories/skillbox/empty_app/main.cpp" "/home/wandsman/repositories/skillbox/cmake-build-debug/CMakeFiles/c__.dir/empty_app/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -22,6 +26,10 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../11.6"
   "../12"
   "../empty_app"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
