1d9f5ce1dfd6a50d983593ae5737a9d6c3ef36e6
wandsman <svenwingame@gmail.com>
2022-01-04 00:47:23 +0300

    task_1 added

--- 18/task_1/main.cpp
+++ 18/task_1/main.cpp
@@ -2,25 +2,29 @@
 #include <vector>
 
 void new_swap(std::vector<int>&a,int *b);
-
 int main() {
-
-
-    std::vector<int> a{1,2,3,4};
+    std::vector<int>a{1,2,3,4};
     int b[]{2,4,6,8};
 
-   new_swap(a,b);
+new_swap(a,b);
 
-    for (int i = 0; i < 4; ++i) std::cout<<a[i];
+    for (int i = 0; i < 4; ++i) {
+        std::cout<<a[i];
+    }
     std::cout<<std::endl;
-    for (int i = 0; i < 4; ++i) std::cout<<b[i];
-
+    for (int i = 0; i < 4; ++i) {
+        std::cout<<b[i];
+    }
 
     return 0;
 }
 
 void new_swap(std::vector<int> &a, int *b) {
-    int x=0;
+    int x{};
+    for (int i = 0; i < 4; ++i) {
 
-    
+        x=a[i];
+        a[i]=*(b+i);
+        *(b+i)=x;
+    }
 }
