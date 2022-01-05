d57b99511cd821d393147f6decaad9ecb4880d5c
wandsman <svenwingame@gmail.com>
2022-01-05 22:51:50 +0300

    task_4 changed

--- 18/task_4/main.cpp
+++ 18/task_4/main.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 
-void foo(long long,int& );
+void foo(long long, int &);
 
 int main() {
     int ans{};
     long long n{9223372036854775806};
-    foo(n,ans);
-    std::cout<<"\n"<<ans;
+    foo(n, ans);
+    std::cout << "\n" << ans;
     return 0;
 }
 
-void foo(long long n, int&ans) {
-    int tmp{};
-    if(n==0){
-        return;
-    }else{
-        tmp=n%10;
-        if (tmp%2==0)
+void foo(long long n, int &ans) {
+    int tmp = n % 10;
+    static int size{};
+    if (n == 0) {
+        if (size == 0) {
             ans++;
-        n/=10;
-        foo(n,ans);
+        }
+        return;
+    } else {
+        if (tmp % 2 == 0)
+            ans++;
+        n /= 10;
+        size++;
+        foo(n, ans);
     }
 }
