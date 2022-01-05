788a7620906b44a04f95462e4b898795004284f3
wandsman <svenwingame@gmail.com>
2022-01-05 21:48:47 +0300

    task_2(3) changed

--- 18/task_2/main.cpp
+++ 18/task_2/main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 
-void jump(int jump_l, int *aj, int &result, int ladder_l = 3, int new_jump = 0, int new_ladder = 0) {
+
+void jump(int jump_l,  int &result, int ladder_l = 3) {
+    static int new_ladder = 0;
     for (int i = 1; i <= jump_l && new_ladder + i <= ladder_l; i++) {
-        aj[new_jump] = i;
+
         if (new_ladder + i == ladder_l) {
             result++;
+            if (new_ladder<0){
+                new_ladder=0;
+            }
             return;
         }
-        jump(jump_l, aj, result, ladder_l, new_jump + 1, new_ladder + i);
+        new_ladder +=i;
+        jump(jump_l,  result, ladder_l);
+        new_ladder-=i;
     }
 };
 
@@ -16,10 +23,7 @@ int main() {
     int jump_l{}, ladder_l{}, result{};
     cout << "длинну прыжка и колличество ступенек через пробел:";
     cin >> jump_l >> ladder_l;
-    int *arrjump = new int[ladder_l];
-
-    jump(jump_l, arrjump, result, ladder_l);
+    jump(jump_l,  result, ladder_l);
     cout << result << endl;
-    delete[] arrjump;
     return 0;
 }
